96f2e621402dcadd4a153fa883cf57216dd1c27e
JorgeJr <jorjaispro@gmail.com>
2023-08-08 23:55:26 -0600

    Finalizacion de la creacion de la Base de datos

--- Base de datos/Script.sql
+++ Base de datos/Script.sql
@@ -1,3 +1,13 @@
+use app_cotizacion;
+SET FOREIGN_KEY_CHECKS = 0;
+drop table if exists departamento;
+drop table if exists pedido;
+drop table if exists roles;
+drop table if exists permisos;
+drop table if exists datos_persona;
+drop table if exists roles_permisos;
+drop table if exists cotizaciones;
+
 create table departamento(
 idDepartamento int primary key not null auto_increment,
 vNombreDep varchar(30) not null,
@@ -27,6 +37,14 @@ idPermiso int primary key not null auto_increment,
 vNombrePermiso varchar(20)
 );
 
+create table roles_permisos(
+idRolPermiso int primary key not null auto_increment,
+idRol int not null,
+idPermiso int not null,
+constraint roles_grupo foreign key (idRol) references roles (idRol),
+constraint permiso_grupo foreign key (idPermiso) references permisos (idPermiso)
+);
+
 create table datos_persona(
 idDatos_persona int primary key not null auto_increment,
 vNombreUsuario varchar(20) not null,
@@ -37,12 +55,19 @@ idRol int not null,
 constraint datos_persona_roles foreign key (idRol) references roles (idRol)
 );
 
-create table roles_permisos(
-idRolPermiso int primary key not null auto_increment,
-idRol int not null,
-idPermiso int not null,
-constraint roles_grupo foreign key (idRol) references roles (idRol),
-constraint permiso_grupo foreign key (idPermiso) references permisos (idPermiso)
+
+create table cotizaciones(
+idCotizacion int primary key not null auto_increment,
+idDepartamento int not null,
+idPedido int not null,
+idDatosPersona int not null,
+dFechaCotizacion date default (date(NOW())),
+constraint departamento_cotizacion foreign key (idDepartamento) references departamento (idDepartamento),
+constraint pedido_cotizacion foreign key (idPedido) references pedido (idPedido),
+constraint persona_cotizacion foreign key (idDatosPersona) references datos_persona (idDatos_Persona)
 );
+
+SET FOREIGN_KEY_CHECKS = 1;
+
 
 
