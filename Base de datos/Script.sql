a402acfd97e088a56fb89e1ce0ff6912e0f592f0
JorgeJr <jorjaispro@gmail.com>
2023-08-10 17:49:36 -0600

    Cambio de las bases de datos

--- Base de datos/Script.sql
+++ Base de datos/Script.sql
@@ -8,22 +8,12 @@ drop table if exists datos_persona;
 drop table if exists roles_permisos;
 drop table if exists cotizaciones;
 
-create table departamento(
-idDepartamento int primary key not null auto_increment,
-vNombreDep varchar(30) not null,
-vNombreColaborador varchar(30) not null,
-vApellidoColaborador varchar(30) not null,
-vPuestoColaborador varchar(30) not null
-);
-
-create table pedido(
-idPedido int primary key not null auto_increment,
-vDescripcion varchar(60) not null,
-iCantidadHombres int not null,
-dPrecioHora decimal(10, 2) not null,
-iHorasDia int not null,
-iDias int not null,
-iPrecioTotal decimal(10, 2) not null
+create table cliente(
+idCliente int primary key not null auto_increment,
+tipo enum('Interno', 'Externo') not null,
+vNombreCliente varchar(30) not null,
+vApellidoCliente varchar(30) not null,
+vDescripcion varchar(100) not null
 );
 
 create table roles(
@@ -45,26 +35,36 @@ constraint roles_grupo foreign key (idRol) references roles (idRol),
 constraint permiso_grupo foreign key (idPermiso) references permisos (idPermiso)
 );
 
-create table datos_persona(
-idDatos_persona int primary key not null auto_increment,
+create table usuario(
+idUsuario int primary key not null auto_increment,
 vNombreUsuario varchar(20) not null,
 vNombre varchar(30) not null,
 vApellido varchar(30) not null,
 vPassword varchar(100) not null,
 idRol int not null,
-constraint datos_persona_roles foreign key (idRol) references roles (idRol)
+constraint usuario_roles foreign key (idRol) references roles (idRol)
 );
 
 
 create table cotizaciones(
 idCotizacion int primary key not null auto_increment,
 idDepartamento int not null,
-idPedido int not null,
 idDatosPersona int not null,
-dFechaCotizacion date default (date(NOW())),
+dFechaCotizacion datetime default CURRENT_TIMESTAMP,
 constraint departamento_cotizacion foreign key (idDepartamento) references departamento (idDepartamento),
-constraint pedido_cotizacion foreign key (idPedido) references pedido (idPedido),
-constraint persona_cotizacion foreign key (idDatosPersona) references datos_persona (idDatos_Persona)
+constraint persona_cotizacion foreign key (idDatosPersona) references usuario (idUsuario)
+);
+
+create table pedido(
+idPedido int primary key not null auto_increment,
+vDescripcion varchar(100) not null,
+iCantidadHombres int not null,
+dPrecioHora decimal(10, 2) not null,
+iHorasDia int not null,
+iDias int not null,
+iPrecioTotal decimal(10, 2) not null,
+idCotizacion int not null,
+constraint pedido_cotizacion foreign key (idCotizacion) references cotizaciones (idCotizacion)
 );
 
 SET FOREIGN_KEY_CHECKS = 1;
