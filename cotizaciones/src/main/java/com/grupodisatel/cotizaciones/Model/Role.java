343ee28d05801e1865afb266fc1fe03627c1ae68
JorgeJr <jorjaispro@gmail.com>
2023-08-10 18:34:48 -0600

    Creacion del objeto Role para el guardado de roles

---
+++ cotizaciones/src/main/java/com/grupodisatel/cotizaciones/Model/Role.java
@@ -0,0 +1,27 @@
+package com.grupodisatel.cotizaciones.Model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name = "roles")
+@ToString @EqualsAndHashCode
+public class Role {
+
+    @Id
+    @Getter @Setter @Column(name = "idrol")
+    private int idRole;
+
+    @Getter @Setter @Column(name = "vnombrerol")
+    private String roleName;
+
+    @Getter @Setter @Column(name = "vpalabraclave")
+    private String keyWord;
+    
+}
