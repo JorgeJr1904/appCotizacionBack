324c45bdab0a245a9961d3510ae25d366920d394
JorgeJr <jorjaispro@gmail.com>
2023-08-11 18:09:04 -0600

    Creacion del Objeto PermissionRole para la tabla permisos_roles

---
+++ cotizaciones/src/main/java/com/grupodisatel/cotizaciones/Model/PermissionsRole.java
@@ -0,0 +1,23 @@
+package com.grupodisatel.cotizaciones.Model;
+
+import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name = "roles_permisos")
+@EqualsAndHashCode @ToString
+public class PermissionsRole {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Getter @Setter @Column(name = "idrolpermiso")
+    private int id;
+
+    @Getter @Setter @Column(name = "idrol")
+    private int idRole;
+
+    @Getter @Setter @Column(name = "idpermiso")
+    private int idPermission;
+}
