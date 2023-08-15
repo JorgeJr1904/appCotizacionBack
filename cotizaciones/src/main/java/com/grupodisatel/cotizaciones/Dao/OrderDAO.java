acdd7c98368e206f1ba4bb47c5fabc5fa76b6a8d
JorgeJr <jorjaispro@gmail.com>
2023-08-15 16:34:24 -0600

    Creacion de OrderDAO para CRUD de los pedidos

---
+++ cotizaciones/src/main/java/com/grupodisatel/cotizaciones/Dao/OrderDAO.java
@@ -0,0 +1,69 @@
+package com.grupodisatel.cotizaciones.Dao;
+
+import com.grupodisatel.cotizaciones.Model.Order;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Repository;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Transactional
+@Repository
+public class OrderDAO {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    public List<Order> getAll(){
+        String sql= "FROM Order WHERE status = 1";
+        return entityManager.createQuery(sql).getResultList();
+    }
+
+    public Order getOrder(int id){
+        return entityManager.find(Order.class, id);
+    }
+
+    public boolean newOrder(Order order){
+        order.setTotalPrice(totalPriceOrder(order));
+        order.setStatus('1');
+        entityManager.persist(order);
+        return true;
+    }
+
+    public boolean updateOrder(Order order){
+        Order order1 = entityManager.find(Order.class,order.getId());
+        if (order1 == null){
+            return false;
+        }
+        else {
+            order1.setDescription(order.getDescription());
+            order1.setMen(order.getMen());
+            order1.setHourPrice(order.getHourPrice());
+            order1.setHourDays(order.getHourDays());
+            order1.setDays(order.getDays());
+            order1.setTotalPrice(totalPriceOrder(order));
+            entityManager.merge(order1);
+            return true;
+        }
+    }
+
+    public boolean deleteOrder(int id){
+        Order order1 = entityManager.find(Order.class, id);
+        if (order1 == null){
+            return false;
+        }
+        else {
+            order1.setStatus('0');
+            entityManager.merge(order1);
+            return true;
+        }
+    }
+
+
+    public BigDecimal totalPriceOrder(Order order){
+        return BigDecimal.valueOf(order.getMen()).multiply(order.getHourPrice()).multiply(BigDecimal.valueOf(order.getHourDays()));
+    }
+
+}
