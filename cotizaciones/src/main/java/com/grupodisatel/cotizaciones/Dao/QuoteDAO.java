d4c746f47fd27c17362f9913542c5d44491ca584
JorgeJr <jorjaispro@gmail.com>
2023-08-15 21:41:18 -0600

    Se termina CRUD de UserDAO para los RESTController

--- cotizaciones/src/main/java/com/grupodisatel/cotizaciones/Dao/QuoteDAO.java
+++ cotizaciones/src/main/java/com/grupodisatel/cotizaciones/Dao/QuoteDAO.java
@@ -1,8 +1,10 @@
 package com.grupodisatel.cotizaciones.Dao;
 
 import com.grupodisatel.cotizaciones.Model.Price;
+import com.grupodisatel.cotizaciones.Model.Role;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.Query;
 import org.springframework.stereotype.Repository;
 
 import javax.transaction.Transactional;
@@ -27,13 +29,30 @@ public class PriceDAO {
     public boolean deletePrice(int id){
         try{
             Price price = entityManager.find(Price.class, id);
-            entityManager.remove(price);
+            price.setStatus('0');
             return true;
         }catch (Exception e){
             return false;
         }
     }
-    public List<Price> getPrice(){
+
+    public boolean updatePrice(int id){
+        try{
+            Price price = entityManager.find(Price.class, id);
+            price.setStatus('0');
+            return true;
+        }catch (Exception e){
+            return false;
+        }
+    }
+    public List<Price> getAdminPrice(){
         return entityManager.createQuery("FROM Price").getResultList();
+    }
+
+    public List<Price> getUserPrice(int id){
+        String jpql = "SELECT e FROM Price e WHERE e.user = :id";
+        Query query = entityManager.createQuery(jpql, Price.class);
+        query.setParameter("id", id);
+        return query.getResultList();
     }
 }
--- cotizaciones/src/main/java/com/grupodisatel/cotizaciones/Dao/UserDAO.java
+++ cotizaciones/src/main/java/com/grupodisatel/cotizaciones/Dao/UserDAO.java
@@ -1,10 +1,15 @@
 package com.grupodisatel.cotizaciones.Dao;
 
+import com.grupodisatel.cotizaciones.Model.Quote;
+import com.grupodisatel.cotizaciones.Model.User;
+import jakarta.persistence.Query;
+import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.stereotype.Repository;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.transaction.Transactional;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.transaction.Transactional;
+import java.util.List;
 
 @Transactional
 @Repository
@@ -13,5 +18,63 @@ public class UserDAO {
     @PersistenceContext
     EntityManager entityManager;
 
+    public boolean newUser(User user){
+            if (duplicateUser(user.getUserName()) > 0){
+                return false;
+            }else {
+                user.setPassword(encryptPass(user.getPassword()));
+                entityManager.persist(user);
+                return true;
+            }
+    }
+
+    public boolean deleteUser(int id){
+        try{
+            User user = entityManager.find(User.class, id);
+            user.setStatus('0');
+            return true;
+        }catch (Exception e){
+            return false;
+        }
+    }
+
+    public boolean updateUser(User user){
+        try{
+            User user1 = entityManager.find(User.class, user.getId());
+            user1.setUserName(user.getUserName());
+            user1.setName(user.getName());
+            user1.setLastname(user.getLastname());
+            user1.setIdRole(user.getIdRole());
+            entityManager.merge(user1);
+            return true;
+        }catch (Exception e){
+            return false;
+        }
+    }
+    public List<Quote> getUsers(){
+
+        return entityManager.createQuery("FROM User").getResultList();
+    }
+
+    //Verification user duplicate
+
+    public int duplicateUser(String userName){
+        String sql= "select findUser(:a)";
+        Query query = entityManager.createNativeQuery(sql);
+        query.setParameter("a",userName);
+        return query.getFirstResult();
+    }
+
+
+
+
+    //encryption and password verification------------------------------------------------------------------------------------------------------------------
+    public String encryptPass(String pass){
+        return BCrypt.hashpw(pass, BCrypt.gensalt());
+    }
+
+    public boolean passMatch(String pass, String hashPass){
+        return BCrypt.checkpw(pass, hashPass);
+    }
 
 }
