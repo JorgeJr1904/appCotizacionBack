b5ebd7d019f185625aa246c51f85c1a57e38fe47
JorgeJr <jorjaispro@gmail.com>
2023-08-16 21:48:35 -0600

    Se realiza un cambio en la funcion PermissionsRole en la cual hay un error de sintaxis en el sql

--- cotizaciones/src/main/java/com/grupodisatel/cotizaciones/Dao/PermissionRoleDAO.java
+++ cotizaciones/src/main/java/com/grupodisatel/cotizaciones/Dao/PermissionRoleDAO.java
@@ -4,6 +4,7 @@ import com.grupodisatel.cotizaciones.Model.Order;
 import com.grupodisatel.cotizaciones.Model.PermissionsRole;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.Query;
 import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Repository;
 
@@ -17,7 +18,7 @@ public class PermissionRoleDAO {
     private EntityManager entityManager;
 
     public List<PermissionsRole> getAll(){
-        String sql= "FROM PermissionsRole WHERE status = 1";
+        String sql= "FROM PermissionsRole WHERE status = '1'";
         return entityManager.createQuery(sql).getResultList();
     }
 
@@ -26,22 +27,29 @@ public class PermissionRoleDAO {
     }
 
     public boolean newPermissionsRole(PermissionsRole permissionsRole){
-        permissionsRole.setStatus('1');
-        entityManager.persist(permissionsRole);
-        return true;
+        if (duplicatePermissionRole(permissionsRole.getIdRole(), permissionsRole.getIdPermission()) == 0){
+            permissionsRole.setStatus('1');
+            entityManager.persist(permissionsRole);
+            return true;
+        }else {
+            return false;
+        }
     }
 
     public boolean updatePermissionRole(PermissionsRole permissionsRole){
         PermissionsRole permissionsRole1 = entityManager.find(PermissionsRole.class,permissionsRole.getId());
-        if (permissionsRole1 == null){
-            return false;
+        if (permissionsRole1 != null) {
+            if (duplicatePermissionRole(permissionsRole.getIdRole(), permissionsRole.getIdPermission()) == 0) {
+                try{
+                    permissionsRole1.setIdRole(permissionsRole.getIdRole());
+                    permissionsRole1.setIdPermission(permissionsRole.getIdPermission());
+                    return true;
+                }catch (Exception e){
+                    return false;
+                }
+            }
         }
-        else {
-            permissionsRole1.setIdRole(permissionsRole.getIdRole());
-            permissionsRole1.setIdPermission(permissionsRole.getIdPermission());
-            entityManager.merge(permissionsRole1);
-            return true;
-        }
+        return false;
     }
 
     public boolean deletePermissionsRole(int id){
@@ -51,8 +59,17 @@ public class PermissionRoleDAO {
         }
         else {
             permissionsRole.setStatus('0');
-            entityManager.merge(permissionsRole);
             return true;
         }
+    }
+
+
+    public int duplicatePermissionRole(int idRole, int idPermission){
+        String jpql = "SELECT count(*) FROM PermissionsRole e WHERE e.idRole = :role AND e.idPermission = :permission";
+        Query query = entityManager.createQuery(jpql);
+        query.setParameter("role", idRole);
+        query.setParameter("permission", idPermission);
+        long count = (long) query.getSingleResult();
+        return (int) count;
     }
 }
