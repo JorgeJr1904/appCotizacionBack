b7e32dad2b4705f1c4fa65906f77e79dfa657f4f
JorgeJr <jorjaispro@gmail.com>
2023-08-16 23:03:37 -0600

    Se realizan cambio a los metodos que aun tenian el nombre de Price a Quote para mejor lectura ademas se agregan nuevas validaciones a los metodos

--- cotizaciones/src/main/java/com/grupodisatel/cotizaciones/Dao/QuoteDAO.java
+++ cotizaciones/src/main/java/com/grupodisatel/cotizaciones/Dao/QuoteDAO.java
@@ -5,8 +5,7 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import jakarta.persistence.Query;
 import org.springframework.stereotype.Repository;
-
-import javax.transaction.Transactional;
+import jakarta.transaction.Transactional;
 
 import java.util.List;
 
@@ -16,45 +15,48 @@ public class QuoteDAO {
 
     @PersistenceContext
     private EntityManager entityManager;
-    public boolean newPrice(Quote price){
+    public boolean newQuote(Quote quote){
         try {
-            entityManager.persist(price);
+            entityManager.persist(quote);
             return true;
         }catch (Exception ex){
             return false;
         }
     }
 
-    public boolean deletePrice(int id){
+    public boolean deleteQuote(int id){
+        Quote quote = entityManager.find(Quote.class, id);
         try{
-            Quote quote = entityManager.find(Quote.class, id);
-            quote.setStatus('0');
-            return true;
+            if (quote == null){
+                return false;
+            }else {
+                quote.setStatus('0');
+                return true;
+            }
         }catch (Exception e){
             return false;
         }
     }
 
-    public boolean updatePrice(Quote quote){
+    public boolean updateQuote(Quote quote){
         try{
             Quote quote1 = entityManager.find(Quote.class, quote.getId());
             quote1.setCustomername(quote.getCustomername());
-            quote1.setCustomerlastname(quote.getCustomername());
-            quote1.setCustomerType(quote.getCustomername());
-            entityManager.merge(quote1);
+            quote1.setCustomerlastname(quote.getCustomerlastname());
+            quote1.setCustomerType(quote.getCustomerType());
             return true;
         }catch (Exception e){
             return false;
         }
     }
-    public List<Quote> getAdminPrice(){
-        return entityManager.createQuery("FROM Price").getResultList();
+    public List<Quote> getAdminQuote(){
+        return entityManager.createQuery("FROM Quote WHERE status = '1'").getResultList();
     }
 
-    public List<Quote> getUserPrice(int id){
-        String jpql = "SELECT e FROM Quote e WHERE e.user = :id";
+    public List<Quote> getUserQuote(int idUser){
+        String jpql = "SELECT e FROM Quote e WHERE e.user = :id AND e.status = '1'";
         Query query = entityManager.createQuery(jpql, Quote.class);
-        query.setParameter("id", id);
+        query.setParameter("id", idUser);
         return query.getResultList();
     }
 }
