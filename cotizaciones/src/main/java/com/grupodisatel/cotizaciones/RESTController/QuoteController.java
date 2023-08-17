f33bb336b62c299a756451fa69e9de00e3ab1723
JorgeJr <jorjaispro@gmail.com>
2023-08-16 23:04:18 -0600

    Se crean los endpoint para agregar a la entidad Quote en la base de datos

---
+++ cotizaciones/src/main/java/com/grupodisatel/cotizaciones/RESTController/QuoteController.java
@@ -0,0 +1,42 @@
+package com.grupodisatel.cotizaciones.RESTController;
+
+import com.grupodisatel.cotizaciones.Dao.QuoteDAO;
+import com.grupodisatel.cotizaciones.Model.Order;
+import com.grupodisatel.cotizaciones.Model.Quote;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/quote")
+public class QuoteController {
+
+    @Autowired
+    private QuoteDAO quoteDAO;
+
+    @RequestMapping(value = "new", method = RequestMethod.POST)
+    public boolean newQuote(@RequestBody Quote quote){
+        return quoteDAO.newQuote(quote);
+    }
+
+    @RequestMapping(value = "delete/{id}", method = RequestMethod.DELETE)
+    public boolean deleteQuote(@PathVariable int id){
+        return quoteDAO.deleteQuote(id);
+    }
+
+    @RequestMapping(value = "update", method = RequestMethod.PUT)
+    public boolean updateQuote(@RequestBody Quote quote){
+        return quoteDAO.updateQuote(quote);
+    }
+
+    @RequestMapping(value = "get/{id}", method = RequestMethod.GET)
+    public List<Quote> getQuote(@PathVariable int id){
+        return quoteDAO.getUserQuote(id);
+    }
+
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    public List<Quote> getUser(){
+        return quoteDAO.getAdminQuote();
+    }
+}
